1aaa8a6e85698054d7448f2acb15131276da0baf
mikee47 <mike@sillyhouse.net>
2021-11-05 16:54:14 +0000

    Comment headers

--- src/MHZ19.cpp
+++ src/MHZ19.cpp
@@ -1,9 +1,21 @@
-/*
-  MHZ19.cpp - MH-Z19 CO2 sensor library for ESP8266 or Arduino
-  version 1.0
-  
-  License MIT
-*/
+/**
+ * MHZ19.cpp
+ *
+ * Copyright 2021 mikee47 <mike@sillyhouse.net>
+ *
+ * This file is part of the Sming-MHZ19 Library
+ *
+ * This library is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, version 3 or later.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this library.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ ****/
 
 #include "MHZ19.h"
 #include <Digital.h>
